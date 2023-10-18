ec7dd88d587c44609ba61623679a54190a6d1848
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-10-18 09:54:45 +0700

    Better Modal CRUD

--- resources/js/Pages/Post/Create.vue
+++ resources/js/Pages/Post/Create.vue
@@ -25,27 +25,42 @@ const form = useForm({
 });
 
 const submit = () => {
-    form.post(route("posts.store"));
-    cModalCreate();
+    form.post(route('posts.store'), {
+        onSuccess: () => {
+            // Setelah proses form selesai dengan sukses, reset form
+            form.reset('title', 'content');
+            
+            // Tutup modal
+            props.cModalCreate();
+        },
+    });
 };
 </script>
 
 <template>
-        <button @click="cModalCreate">Close Modal</button>
-        <div class="py-12">
+    <div class="">
+        <div class="">
+            <button type="button" @click="cModalCreate" class="absolute top-3 right-4 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <h1 class="absolute top-4 left-6 text-gray-400">Create Post</h1>
+            <div class="pt-12">
             <div class="mx-auto max-w-7xl">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
                         <form @submit.prevent="submit">
                             <div>
-                                <InputLabel for="title" value="Title" />
+                                <InputLabel for="titleCreate" value="Title" />
 
                                 <TextInput
-                                    id="title"
+                                    id="titleCreate"
                                     type="text"
                                     class="mt-1 block w-full"
                                     v-model="form.title"
-                                    required
+                                    
                                     autofocus
                                     autocomplete="username"
                                 />
@@ -57,16 +72,15 @@ const submit = () => {
                             </div>
 
                             <div class="my-6">
-                                <label
-                                    for="content"
-                                    class="block mb-2 text-sm font-medium text-gray-900"
-                                    >Content</label
-                                >
+                                <InputLabel
+                                    for="contentCreate"
+                                    value="Content"
+                                    />
                                 <textarea
                                     type="text"
                                     v-model="form.content"
                                     name="content"
-                                    id=""
+                                    id="contentCreate"
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                 ></textarea>
 
@@ -84,9 +98,12 @@ const submit = () => {
                             >
                                 Submit
                             </PrimaryButton>
+                            
                         </form>
                     </div>
                 </div>
             </div>
         </div>
+        </div>
+    </div>
 </template>
--- resources/js/Pages/Post/Edit.vue
+++ resources/js/Pages/Post/Edit.vue
@@ -7,33 +7,47 @@ import PrimaryButton from "@/Components/PrimaryButton.vue";
 import { Head, useForm } from "@inertiajs/vue3";
 
 const props = defineProps({
-    post: {
-        type: Object,
-        default: () => ({}),
+    modalEdit: {
+        type: Boolean
     },
-    CModalEdit: {
+    cModalEdit: {
         type: Function,
     },
     idPost: {
-        type: Number
+        type: Object
     }
 });
 
 const form = useForm({
-    id_post: props.idPost,
-    title: props.post.title,
-    content: props.post.content,
+    id_post: props.idPost.id_post,
+    title: props.idPost.title,
+    content: props.idPost.content,
 });
 
 
 const submit = () => {
-    form.put(route("posts.update", props.idPost));
-    
+    form.put(route("posts.update", props.idPost.id_post), {
+        onSuccess: () => {
+            form.reset('id_post', 'title', 'content');
+            
+            // Tutup modal
+            props.cModalEdit();
+        },
+    })
 };
 </script>
 
 <template>
-        <div class="py-12">
+    <div class="">
+        <div class="">
+            <button type="button" @click="cModalEdit" class="absolute top-3 right-4 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <h1 class="absolute top-4 left-6 text-gray-400">Edit Post</h1>
+            <div class="pt-12">
             <div class="mx-auto max-w-7xl">
                 <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                     <div class="p-6 bg-white border-b border-gray-200">
@@ -47,8 +61,7 @@ const submit = () => {
                                 <InputError class="mt-2" :message="form.errors.title" />
                             </div>
                             <div class="my-6">
-                                <label for="slug"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Content</label>
+                                <InputLabel for="slug" value="Content"/>
                                 <textarea type="text" v-model="form.content" name="content" id=""
                                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"></textarea>
 
@@ -65,4 +78,6 @@ const submit = () => {
                 </div>
             </div>
         </div>
+        </div>
+    </div>
 </template>
